83a453cf0b5744219ec28c1cd8d4a5a63a71dca1
Rana-Osama <90419549+Rana-Osama@users.noreply.github.com>
2023-10-16 20:42:55 +0300

    + shared preference

--- lib/SplashScreen.dart
+++ lib/SplashScreen.dart
@@ -1,19 +1,33 @@
 import 'package:flutter/material.dart';
 import 'package:islami_c9_sat/providers/SettingsProvider.dart';
 import 'package:provider/provider.dart';
-import 'MyThemeData.dart';
 
 class SplashScreen extends StatelessWidget {
+  static const String routeName = 'splash';
   const SplashScreen({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    var provider = Provider.of<SettingsProvider>(context);
-    return Scaffold(
-      body: provider.currentTheme == ThemeMode.dark
-          ? Image.asset('assets/images/splash_dark.png',
-              height: double.infinity, width: double.infinity)
-          : Image.asset('assets/images/splash.png',
-              height: double.infinity, width: double.infinity),
+    var provider = SettingsProvider();
+    return FutureBuilder(
+      future: provider.loadTheme(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting){
+          return Center(
+            child: CircularProgressIndicator(),
+          );
+        }
+        else {
+          String? theme = provider.getTheme();
+          return Scaffold(
+            body: theme == 'Light'
+                ? Image.asset('assets/images/splash.png',
+                height: double.infinity, width: double.infinity)
+                : Image.asset('assets/images/splash_dark.png',
+                height: double.infinity, width: double.infinity),
+          );
+        }
+      },
+
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,65 +1,76 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
-import 'package:islami_c9_sat/HadethDetails/HadethDetailsScreen.dart';
-import 'package:islami_c9_sat/MyThemeData.dart';
-import 'package:islami_c9_sat/chapter_details/ChapterDetails.dart';
-import 'package:islami_c9_sat/home/HomeScreen.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
-import 'package:islami_c9_sat/providers/SettingsProvider.dart';
 import 'package:provider/provider.dart';
 
 import 'SplashScreen.dart';
+import 'MyThemeData.dart';
+import 'chapter_details/ChapterDetails.dart';
+import 'HadethDetails/HadethDetailsScreen.dart';
+import 'home/HomeScreen.dart';
+import 'providers/SettingsProvider.dart';
 
-void main()  {
+void main() {
   WidgetsFlutterBinding.ensureInitialized();
-  runApp(const MaterialApp(
-    home:  SplashScreen(),
-  ));
-  Timer(const Duration(seconds: 2), () async{
+  runApp(
+    const MaterialApp(
+      home: SplashScreen(),
+    ),
+  );
+  Timer(const Duration(seconds: 2), () async {
     var provider = SettingsProvider();
     await provider.loadTheme();
     await provider.loadLocal();
-    runApp(ChangeNotifierProvider(
-        create: (buildContext) => provider, child: MyApp()));
+    runApp(
+      MultiProvider(
+        providers: [
+          ChangeNotifierProvider<SettingsProvider>(
+            create: (_) => provider,
+          ),
+        ],
+        child: MyApp(),
+      ),
+    );
   });
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key});
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    //search forr settings provider inside widgets tree
-    //by default listen for changes in settings provider
-    SettingsProvider settingsProvider = Provider.of<SettingsProvider>(context);
-    return MaterialApp(
-      title: 'Islami App',
-      theme: MyThemeData.LightTheme,
-      darkTheme: MyThemeData.DarkTheme,
-      //data in settings provider
-      themeMode: settingsProvider.currentTheme,
-      routes: {
-        HomeScreen.routeName: (_) => HomeScreen(),
-        ChapterDetailsScreen.routeName: (_) => ChapterDetailsScreen(),
-        HadethDetailsScreen.routeName: (_) => HadethDetailsScreen(),
-      },
-      initialRoute: HomeScreen.routeName,
+    return Consumer<SettingsProvider>(
+      builder: (context, settingsProvider, _) {
+        return MaterialApp(
+          title: 'Islami App',
+          theme: MyThemeData.LightTheme,
+          darkTheme: MyThemeData.DarkTheme,
+          //data in settings provider
+          themeMode: settingsProvider.currentTheme,
+          routes: {
+            SplashScreen.routeName: (_) => SplashScreen(),
+            HomeScreen.routeName: (_) => HomeScreen(),
+            ChapterDetailsScreen.routeName: (_) => ChapterDetailsScreen(),
+            HadethDetailsScreen.routeName: (_) => HadethDetailsScreen(),
 
-//can be written => localizationsDelegates: AppLocalizations.localizationsDelegates,
-      localizationsDelegates: [
-        AppLocalizations.delegate,
-        GlobalMaterialLocalizations.delegate,
-        GlobalWidgetsLocalizations.delegate,
-        GlobalCupertinoLocalizations.delegate,
-      ],
-      supportedLocales: [
-        Locale('en'), // English
-        Locale('ar'), // Arabic
-      ],
-      locale: Locale(settingsProvider.currentLocale),
+          },
+          initialRoute: HomeScreen.routeName,
+          localizationsDelegates: [
+            AppLocalizations.delegate,
+            GlobalMaterialLocalizations.delegate,
+            GlobalWidgetsLocalizations.delegate,
+            GlobalCupertinoLocalizations.delegate,
+          ],
+          supportedLocales: [
+            Locale('en'), // English
+            Locale('ar'), // Arabic
+          ],
+          locale: Locale(settingsProvider.currentLocale),
+        );
+      },
     );
   }
 }
